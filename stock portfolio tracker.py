9081e228582082e448e037305b9c3fceb56edb19
Bindu S C <bindusc2@gmail.com>
2026-09-01 15:14:50 +0530

    Update stock portfolio tracker.py

--- stock portfolio tracker.py
+++ stock portfolio tracker.py
@@ -3,13 +3,10 @@ stocks = {
     "TSLA": 250,
     "GOOGL": 150
 }
-
 stock_name = input("Enter stock name: ")
 quantity = int(input("Enter quantity: "))
-
 price = stocks[stock_name]
 total = price * quantity
-
 print("Stock Name:", stock_name)
 print("Stock Price:", price)
 print("Quantity:", quantity)
